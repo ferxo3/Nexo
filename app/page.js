85aad23213810a00ff9a9e19d369c4419a070a9f
Ferxo <fernandohurtado@Ferxos-iMac.local>
2024-10-09 01:47:46 +0200

    first commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Home5 from "./(homes)/home-5/page"; // Asegúrate de que esta ruta sea correcta y apunta a home-5
+
+export const metadata = {
+  title:
+    "Home 5 || Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+  description:
+    "Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+};
+
+export default function HomePage5() {
+  return (
+    <>
+      <Home5 />
+    </>
+  );
+}
